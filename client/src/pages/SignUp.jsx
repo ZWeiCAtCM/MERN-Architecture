ef46b7c2637bed1d3ffea31d7f1ea3f09c595480
Zhaowei Chen <meanolmoon@gmail.com>
2023-12-17 02:12:48 -0500

    Sign up UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import {Link} from 'react-router-dom';
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className="text-3xl text-center font-semibold my-7">Sign up</h1>
+      <form className='flex flex-col gap-4'>
+        <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username'/>
+        <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email'/>
+        <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password'/>
+        <button className='bg-slate-600 text-white p-3 rounded-lg uppercase hover:bg-slate-500 disabled:hover:bg-slate-300'>Sign Up</button>
+      </form>
+      <div className='flex gap-2 mt-5 mx-auto'>
+        <p>Have an account?</p>
+        <Link to={"/sign-in"}>
+          <span>Sign in</span>
+        </Link>
+      </div>
+    </div>
   )
 }
