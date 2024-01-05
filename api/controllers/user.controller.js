164f61db51ec576c34429ea3e0371bf4557e8e05
Zhaowei Chen <meanolmoon@gmail.com>
2024-01-05 04:20:12 -0500

    add delete user

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -39,7 +39,8 @@ export const deleteUser = async (req,res,next) => {
   if (req.user.id !== req.params.id) return next(errorHandler(401, 'You can only delete your own account!!'));
   try {
     await User.findByIdAndDelete(req.params.id);
-    res.status(200).json('User has been deleted!')
+    res.clearCookie('access_token');
+    res.status(200).json('User has been deleted!');
   } catch (error) {
     next(error);
   }
