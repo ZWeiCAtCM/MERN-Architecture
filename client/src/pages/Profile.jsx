b0bd6b20f790d4caf837b56dec788668d0b99f84
Zhaowei Chen <meanolmoon@gmail.com>
2024-01-08 05:08:31 -0500

    complete delete user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -141,6 +141,25 @@ export default function Profile() {
       setShowListingsError(true);
     }
   };
+
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -232,12 +251,16 @@ export default function Profile() {
         {showListingsError ? "Error showing listings" : ""}
       </p>
 
-      {userListings &&
-        userListings.length > 0 &&
+      {userListings && userListings.length > 0 && (
         <div className="flex flex-col gap-4">
-          <h1 className="text-center my-7 text-2xl font-semibold">Your Listings</h1>
+          <h1 className="text-center my-7 text-2xl font-semibold">
+            Your Listings
+          </h1>
           {userListings.map((listing) => (
-            <div key={listing._id} className="border rounded-lg p-3 flex justify-between items-center gap-4">
+            <div
+              key={listing._id}
+              className="border rounded-lg p-3 flex justify-between items-center gap-4"
+            >
               <Link
                 to={`/listings/${listing._id}`}
                 className="flex justify-between items-center gap-4"
@@ -252,13 +275,20 @@ export default function Profile() {
                 </p>
               </Link>
               <div className="flex flex-col items-center">
-                <button className="text-red-700 uppercase hover:shadow-lg  rounded-lg px-2">Delete</button>
-                <button className="text-green-700 uppercase hover:shadow-lg  rounded-lg px-2">Edit</button>
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 uppercase hover:shadow-lg  rounded-lg px-2"
+                >
+                  Delete
+                </button>
+                <button className="text-green-700 uppercase hover:shadow-lg  rounded-lg px-2">
+                  Edit
+                </button>
               </div>
             </div>
           ))}
         </div>
-      }
+      )}
     </div>
   );
 }
