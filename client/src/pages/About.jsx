d1989df17d0fb200c38e2a038e69b2580bf5e6b6
Zhaowei Chen <meanolmoon@gmail.com>
2024-01-11 00:47:47 +0800

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About the project</h1>
+      <p className='mb-4 text-slate-700'>This is an exercise project of MERN tech stack by Zhaowei Chen at Carnegie Mellon University.</p>
+      <p className='mb-4 text-slate-700'>Content listings are projects the author did or participated in when he studied architecture and worked as an architectural designer</p>
+    </div>
   )
 }
